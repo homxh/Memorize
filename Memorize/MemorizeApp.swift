80d9e508674a914bb404622ba2295edae1f03d1c
Howard <homxh2@gmail.com>
2024-05-06 21:17:19 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Howard's Mac mini on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
